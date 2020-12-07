064f378ef16ecac844188cc05fdf4751eefd9a85
Mahdi AlKhalaf <m@mahdi.pro>
2020-12-07 11:22:57 +0300

    feat: Implement barcode scanning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {AppBar, Box, TextField, Typography} from "@material-ui/core";
+import {BrowserMultiFormatReader} from "@zxing/library";
+import _ from "lodash";
+
 
 export default function App() {
+    const [barcode, setBarcode] = useState("");
+
+    useEffect(() => {
+        const barcodeReader = new BrowserMultiFormatReader();
+        barcodeReader.listVideoInputDevices()
+            .then(videoDevices => {
+                if (videoDevices.length < 1) {
+                    alert("No camera detected")
+                    return
+                }
+                barcodeReader.decodeFromVideoDevice(
+                    videoDevices[videoDevices.length - 1].deviceId,
+                    "scannerVideo",
+                    result => {
+                        const isBarcodeNew = result
+                            && !_.isEmpty(result.getText().trim())
+                            && result.getText() !== barcode;
+                        if (isBarcodeNew) {
+                            new Audio("assets/barcode-scanner.mp3").play()
+                                .then(() => {
+                                    setBarcode(result.getText());
+                                });
+                        }
+                    }
+                );
+            })
+            .catch(reason => {
+                console.log(`error: ${reason}`);
+                alert(`error: ${reason}`);
+            });
+        return function clean() {
+            barcodeReader.reset()
+        }
+    })
+
     return (
         <Box height="100vh">
             <AppBar position="static">
@@ -32,7 +70,7 @@ export default function App() {
                 </Box>
                 <Box>
                     <Typography variant="body1">Barcode</Typography>
-                    <TextField variant="outlined" fullWidth/>
+                    <TextField variant="outlined" fullWidth value={barcode} disabled={true}/>
                 </Box>
             </Box>
         </Box>
