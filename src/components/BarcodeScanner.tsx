7f6e72d6869ff0dd8ee8016577f72d00c30cf10d
Mahdi AlKhalaf <m@mahdi.pro>
2020-12-11 04:26:35 +0300

    refactor: Refactor BarcodeScanner to use refs

--- src/components/BarcodeScanner.tsx
+++ src/components/BarcodeScanner.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Box, InputLabel, MenuItem, Select, TextField} from "@material-ui/core";
 import {BrowserMultiFormatReader} from "@zxing/library";
 import _ from "lodash";
@@ -6,22 +6,22 @@ import {FormattedMessage, useIntl} from "react-intl";
 
 export default function BarcodeScanner() {
     const [barcode, setBarcode] = useState("");
-    const [barcodeReader, setBarcodeReader] = useState<BrowserMultiFormatReader | null>(null);
-    const [videoDevices, setVideoDevices] = useState<Array<MediaDeviceInfo>>([]);
     const [selectedVideoDeviceId, setSelectedVideoDeviceId] = useState<string | null>(null);
+    const barcodeReaderRef = useRef<BrowserMultiFormatReader | null>(null);
+    const videoDevicesRef = useRef<Array<MediaDeviceInfo>>([]);
+    const videoElementRef = useRef<HTMLVideoElement | null>(null);
 
     const intl = useIntl();
 
     useEffect(() => {
-        const barcodeReader = new BrowserMultiFormatReader();
-        setBarcodeReader(barcodeReader);
-        barcodeReader.listVideoInputDevices()
+        barcodeReaderRef.current = new BrowserMultiFormatReader();
+        barcodeReaderRef.current.listVideoInputDevices()
             .then(videoDevices => {
                 if (videoDevices.length < 1) {
                     alert("No camera detected")
                     return
                 }
-                setVideoDevices(videoDevices);
+                videoDevicesRef.current = videoDevices;
                 setSelectedVideoDeviceId(videoDevices[videoDevices.length - 1].deviceId)
             })
             .catch(reason => {
@@ -29,15 +29,15 @@ export default function BarcodeScanner() {
                 alert(`error: ${reason}`);
             });
         return function clean() {
-            barcodeReader.reset()
+            barcodeReaderRef.current?.reset()
         }
     }, [])
 
     useEffect(() => {
         if (selectedVideoDeviceId) {
-            barcodeReader?.decodeFromVideoDevice(
+            barcodeReaderRef.current?.decodeFromVideoDevice(
                 selectedVideoDeviceId,
-                "scannerVideo",
+                videoElementRef.current,
                 result => {
                     const isBarcodeNew = result
                         && !_.isEmpty(result.getText().trim())
@@ -51,21 +51,20 @@ export default function BarcodeScanner() {
                 }
             )
             return () => {
-                barcodeReader?.stopContinuousDecode();
+                barcodeReaderRef.current?.reset();
             }
         }
-    }, [selectedVideoDeviceId, barcode, barcodeReader])
+    }, [selectedVideoDeviceId, barcode])
 
     return (
         <Box
             display="grid"
-            gridTemplateRows="auto auto auto"
             gridGap={10}
             justifyContent="center"
             p={2}
         >
             <Box>
-                {(videoDevices?.length ?? 0) > 0 && selectedVideoDeviceId && <>
+                {(videoDevicesRef.current?.length ?? 0) > 0 && selectedVideoDeviceId && <>
                     <InputLabel id="cameraInputLabel">
                         <FormattedMessage
                             id="camera"
@@ -78,7 +77,7 @@ export default function BarcodeScanner() {
                         value={selectedVideoDeviceId}
                         onChange={e => setSelectedVideoDeviceId(e.target.value as string)}
                     >
-                        {videoDevices?.map(device =>
+                        {videoDevicesRef.current?.map(device =>
                             <MenuItem value={device.deviceId} key={device.deviceId}>{device.label}</MenuItem>
                         )}
                     </Select>
@@ -91,7 +90,7 @@ export default function BarcodeScanner() {
                 border="2px solid black"
                 borderRadius={5}
             >
-                <video id="scannerVideo" width="100%" height="100%"/>
+                <video ref={videoElementRef} width="100%" height="100%"/>
             </Box>
             <Box>
                 <TextField
